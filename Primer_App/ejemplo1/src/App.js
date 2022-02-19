8a18f00c021876cc44ccc285931907fd1815a788
eldaniell01 <amontepequem@miumg.edu.gt>
2022-02-19 02:28:34 -0600

    ejemplo1_1

---
+++ Primer_App/ejemplo1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola mundo</h1>
+      <h1>hola mundo</h1>
+      <h1>hola mundo</h1>
+      <h1>hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
